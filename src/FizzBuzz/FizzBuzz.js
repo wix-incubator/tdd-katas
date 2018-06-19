f72c5645fab9e4dba839b929c533b099d8c83e6b
Daniel Zlotin <zlotindaniel@gmail.com>
2018-06-19 20:49:44 +0300

    added FizzBuzz, documentation needed

---
+++ src/FizzBuzz/FizzBuzz.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+
+const DEFAULT_RULES = {
+  3: 'Fizz',
+  5: 'Buzz'
+};
+
+class FizzBuzz {
+  constructor(rules = {}) {
+    this.rules = { ...DEFAULT_RULES, ...rules };
+  }
+
+  fizzBuzz(n) {
+    let result = '';
+    _.forEach(this.rules, (word, num) => {
+      if (n % num === 0) {
+        result += word;
+      }
+    });
+    if (result) {
+      return result;
+    } else {
+      return String(n);
+    }
+  }
+
+  generate(range = { from: 1, to: 15 }) {
+    const result = [];
+    for (let i = range.from; i <= range.to; i++) {
+      result.push(this.fizzBuzz(i))
+    }
+    return result;
+  }
+}
+
+module.exports = { FizzBuzz };
